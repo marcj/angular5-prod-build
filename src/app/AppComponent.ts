16ddd29b8be60ea406ed35d282d1223224e0f8dc
Marc J. Schmidt <marc@marcjschmidt.de>
2018-03-15 20:30:47 +0100

    init

---
+++ src/app/AppComponent.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) by Marc J. Schmidt <marc@marcjschmidt> - all rights reserved.
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    template: ''
+})
+export default class AppComponent {
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) by Marc J. Schmidt <marc@marcjschmidt> - all rights reserved.
+ */
+
+import {NgModule} from '@angular/core';
+import AppComponent from "./AppComponent";
+
+@NgModule({
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
